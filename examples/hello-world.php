e30e168ef9f119074dba82bc9880ea4a45836cf8
Alan Pinstein <apinstein@mac.com>
2021-09-07 18:08:43 -0400

    Clean up example

--- examples/hello-world.php
+++ examples/hello-world.php
@@ -2,25 +2,28 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Swoole\Coroutine;
-use Swoozle\RunUtils;
-
 Swoole\Runtime::enableCoroutine(SWOOLE_HOOK_ALL);
 
 Co\run(function() {
     // You can call a go() routine with arguments
     $str = "Hello, World!";
     go(function($str) {
-        RunUtils::registerCoroutine("F1");
+        Swoozle\RunUtils::registerCoroutine("F1");
 
         print "Go says: {$str}".PHP_EOL;
+
+        sleep(2);
     }, $str);
 
     // Or, you can "use" variables.
     go(function() use ($str) {
+        Swoozle\RunUtils::registerCoroutine("F2");
+
         print "Go says: {$str} (via use)".PHP_EOL;
+
+        sleep(2);
     });
 
-    RunUtils::watch();
+    Swoozle\RunUtils::watch();
 });
 
